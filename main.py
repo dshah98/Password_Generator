4d6706bb332f98015c60b19c6897aef093b530ba
Dhairya Shah <dshah98@users.noreply.github.com>
2020-08-16 15:59:21 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import string
+import random
+
+if __name__ == "__main__":
+    s1 = string.ascii_lowercase
+    s2 = string.ascii_uppercase
+    s3 = string.digits
+    s4 = string.punctuation
+    plen = int(input("Enter password length: "))
+
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+    # random.shuffle(s)
+    # print("Your password is: ")
+    # print("".join(s[0:plen]))
+
+    ''' ANOTHER METHOD TO DO '''
+
+    print("Your password is: ")
+    print("".join(random.sample(s, plen)))
